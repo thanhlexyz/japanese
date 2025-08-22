90eb89bc85bfa90157e8125a83282ea303483ab6
Thanh Le <hi@thanhle.xyz>
2025-08-23 08:28:18 +0900

    script to add n3 vocab faster

---
+++ n3_vocab.py
@@ -0,0 +1,40 @@
+from googletrans import Translator
+import pandas as pd
+import pykakasi
+import asyncio
+import tqdm
+
+config = '''#--decks immediate,daily,weekly,monthly,quarterly,yearly
+#--intervals 0,1440,10080,43200,129600,518400
+#--showcolumns JP
+#--showcolumns JP,HIRAGANA,VN,MEANING'''
+
+names = ['JP', 'HIRAGANA', 'MEANING', 'VN']
+
+data = {}
+for name in names:
+    data[name] = []
+
+words = open('n3_vocab.txt').read().strip().split('\n')
+
+kks = pykakasi.kakasi()
+
+async def translate(text):
+    async with Translator() as translator:
+        result = await translator.translate(word, src='ja', dest='en')
+    return result.text
+
+for word in tqdm.tqdm(words):
+    hiragana = kks.convert(word)[0]['hira']
+    meaning = asyncio.run(translate(word))
+    print(word, hiragana, meaning)
+    data['JP'].append(word)
+    data['HIRAGANA'].append(hiragana)
+    data['MEANING'].append(meaning)
+    data['VN'].append('')
+
+df = pd.DataFrame(data)
+df.to_csv('n3_vocab.tsv', index=None, sep='\t')
+
+with open('n3_vocab.tsv', 'a') as fp:
+    fp.write(config)
